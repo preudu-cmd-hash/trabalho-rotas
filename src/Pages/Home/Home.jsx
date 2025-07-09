7658087c61e58eb2d08adac54685ddddf781f3ec
Pedro <pedrohenrique010123456@gmail.com>
2025-07-09 09:46:26 -0300

    passa o id para o details com input

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 export const Home = () => {
+  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({
+    name: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+    navigate(`/details/${formValues.message}`, { state: formValues });
+  };
   return (
-    <div>
-      <h1>Oi</h1>
-    </div>
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nome do produto</label>
+          <input type="text" name="name" onChange={handleChange} />
+          <br />
+          <label htmlFor="message">Id do produto</label>
+          <input type="text" name="message" onChange={handleChange} />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </>
   );
 };
